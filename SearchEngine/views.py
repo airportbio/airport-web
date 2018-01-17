7e609fa718c3dfdab65dc6f3a4f07a182289fc59
kasramvd <kasraavand@gmail.com>
2018-01-17 14:43:49 +0330

    sync the view with new pagination modules

--- SearchEngine/views.py
+++ SearchEngine/views.py
@@ -3,17 +3,18 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.template.loader import render_to_string
-from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse, StreamingHttpResponse
+from django.template import Context, Template
 from django.views.decorators.csrf import csrf_exempt
 from django.core.cache import cache
-from SearchEngine.lib.utils import FindSearchResult
+from SearchEngine.lib.utils import FindSearchResult, Paginator
 from .models import SearchQuery, Recommendation, ServerName, SuggestedServers
 from .forms import SearchForm, SuggestServer
 from itertools import islice
 from collections import Counter
 import json
 from datetime import datetime
+
 
 def home(request):
     recommendations = Recommendation.objects.all()
@@ -39,26 +40,29 @@ def signup_view(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
+
+def generate_template(all_result, error, user):
+    t = Template(open('SearchEngine/templates/SearchEngine/page_format.html').read())
+    
+    while True:
+        if not all_result.has_next():
+            print("break")
+            break
+        c = Context(
+        {'all_results': all_result,
+        'error': error,
+        'founded_results': 'X',
+        'user': user,
+        'selected_len': True,
+        'show_image': False,})
+        yield t.render(c)
+
 @csrf_exempt
 def search_result(request, page=1):
-    # query = get_object_or_404(Search, pk=pk)
     global all_result
-    global founded_results
     keyword = request.POST.get('keyword')
-    if keyword is None:
-        # Changing the page
-        page = request.GET.get('page', 1)
-        paginator = Paginator(all_result, 20)
-        try:
-            results = paginator.page(page)
-        except PageNotAnInteger:
-            results = paginator.page(1)
-        except EmptyPage:
-            results = paginator.page(paginator.num_pages)
-        error = None
-    else:
+    if keyword is not None:
         selected = json.loads(request.POST.get('selected'))
-        # csrftoken = request.POST.get('csrfmiddlewaretoken')
         try:
             search_model = SearchQuery()
             search_model.user = request.user
@@ -68,70 +72,61 @@ def search_result(request, page=1):
             pass
         searcher = FindSearchResult(keyword=keyword, servers=selected, user=request.user)
         try:
-            start_time = datetime.now()
-            all_result = searcher.find_result()
-            print("execution time -- > {} ".format(datetime.now() - start_time))
-            paginator = Paginator(all_result, 20)
-            results = paginator.page(1)
-            # cache.set('search_result', all_result, None)
             error = False
-            founded_results = len(all_result)
-        except ValueError as exc: 
-            # invalid keyword
+            # start_time = datetime.now()
+            all_result = Paginator(searcher.find_result(),
+                                   range_frame=2,
+                                   rows_number=20)
+            # print("execution time -- > {} ".format(datetime.now() - start_time))
+        except ValueError as exc:
+            # invalid keywords
             error = """INVALID KEYWORD:
             Your keyword contains invalid notations!
             {}""".format(exc)
-            print(error)
-            results = paginator.page(0)
-    index = results.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 3 if index >= 3 else 0
-    end_index = index + 3 if index <= max_index - 3 else max_index
-    page_range = list(paginator.page_range)[start_index:end_index]
-    if keyword is None:
-        return render(request, 'SearchEngine/page_format.html',
-                            {'all_results': results,
-                             'error': error,
-                             'founded_results': founded_results,
-                             'user': request.user,
-                             'page_range': page_range,
-                             'selected_len': True,
-                             'show_image': False})
-    else:
+
         html = render_to_string('SearchEngine/page_format.html',
-                            {'all_results': results,
-                             'error': error,
-                             'founded_results': founded_results,
-                             'user': request.user,
-                             'page_range': page_range,
-                             'selected_len': len(selected),
-                             'show_image': False})
-        return HttpResponse(json.dumps({'html': html}), content_type="application/json")
+                                {'all_results': all_result,
+                                 'page': all_result[1],
+                                 'error': error,
+                                 'founded_results': 'X',
+                                 'user': request.user,
+                                 'selected_len': True,
+                                 'show_image': False,
+                                 'is_other_pages':all_result.has_other_pages()})
+
+        return HttpResponse(json.dumps({'html': html}),
+                            content_type="application/json")
+    else:
+        page = int(request.GET.get('page', 1))
+        return render(request, 
+                      'SearchEngine/page_format.html',
+                       {'all_results': all_result,
+                        'page': all_result[page],
+                        'error': False,
+                        'founded_results': 'X',
+                        'user': request.user,
+                        'selected_len': True,
+                        'show_image': False,
+                        'is_other_pages':all_result.has_other_pages()})
 
 @login_required
 def recom_redirect(request, keyword):
     servers = ServerName.objects.all()
     selected = {s.name: s.path for s in servers}
     searcher = FindSearchResult(keyword=keyword, servers=selected, user=request.user)
-    all_result = list(searcher.find_result())
-    paginator = Paginator(all_result, 20)
-    results = paginator.page(1)
-    error = False
-    founded_results = len(all_result)
-    index = results.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 3 if index >= 3 else 0
-    end_index = index + 3 if index <= max_index - 3 else max_index
-    page_range = list(paginator.page_range)[start_index:end_index]
-    return render(request, 'SearchEngine/page_format.html',
-                            {'all_results': results,
-                             'error': error,
-                             'founded_results': founded_results,
-                             'user': request.user,
-                             'page_range': page_range,
-                             'selected_len': len(selected),
-                             'show_image': False})
-
+    all_result = Paginator(searcher.find_result(),
+                                   range_frame=2,
+                                   rows_number=20)
+    return render(request, 
+                  'SearchEngine/page_format.html',
+                  {'all_results': all_result,
+                   'page': all_result[1],
+                   'error': False,
+                   'founded_results': 'X',
+                   'user': request.user,
+                   'selected_len': True,
+                   'show_image': False,
+                   'is_other_pages':all_result.has_other_pages()})
 @csrf_exempt
 def search(request):
     search_form = SearchForm()
