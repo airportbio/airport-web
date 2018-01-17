1d79dcdd4e378398a7c2141d21adde417c5d3b3b
kasramvd <kasraavand@gmail.com>
2018-01-17 18:01:08 +0330

    add compatibilities with django-2 and update login url to add form with captcha

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'searchengine'
 urlpatterns = [
     url(r'^$', views.search, name='search'),
     url(r'^search_result/.*$', views.search_result, name='search_result'),
