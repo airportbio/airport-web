538fb9fd23cf8051aeda847b7a56bfcd5ae8792f
kasramvd <kasraavand@gmail.com>
2018-02-20 03:14:18 +0330

    update initializers to handle new metalink fileds

--- SearchEngine/lib/database_initializer.py
+++ SearchEngine/lib/database_initializer.py
@@ -6,7 +6,7 @@ import glob
 import re
 from string import punctuation
 from django.db import transaction
-syspath.append(ospath.join(ospath.expanduser("~"), 'Desktop/airport-web'))
+syspath.append(ospath.join(ospath.expanduser("~"), 'airport-web'))
 environ.setdefault("DJANGO_SETTINGS_MODULE", "Airport.settings")
 setup()
 from SearchEngine.models import ServerName, WordNet, Server, Path
@@ -21,12 +21,25 @@ class Initializer:
         self.metadata_links = self.load_metadata(kwargs['metadata'])
         self.server_names = self.load_server_names()
         self.punc_regex = re.compile(r'[{}]'.format(re.escape(punctuation)))
+        self. meta_path = kwargs['meta_path']
+        self.metapath_dict = self.get_meta_path()
+
 
     def __call__(self):
         print("Initializing wordnets...")
         self.add_wordnets()
         print("Initializing server_names...")
         self.add_server_names()
+
+    def get_meta_path(self):
+        result = {}
+        for name in glob.glob(self.meta_path + '/*.json'):
+            with open(name) as f:
+                d = json.load(f)
+            name = ospath.splitext(name)[0].split('/')[-1]
+            result[name] = d
+        return result
+
 
     def load_metadata(self, filepath):
         with open(filepath) as f:
@@ -42,20 +55,22 @@ class Initializer:
         WordNet.objects.bulk_create(all_models)
     
     @transaction.atomic
-    def initial_path_model(self, name, data):
+    def initial_path_model(self, name, data, meta):
         for d in data:
             path = Path()
             path.path = d['path']
             path.files = d['files']
             path.keywords = d['keywords']
             path.metadata = self.metadata_links[name]
+            path.meta_path = meta.get(d['path'], [])
             path.server_name = name
             path.save()
 
     def create_server_models(self):
         all_models = {}
         for name, data in self.load_servers():
-            self.initial_path_model(name, data)
+            meta_path_file = self.metapath_dict[ospath.split(name)[-1]] 
+            self.initial_path_model(name, data, meta_path_file)
             query = Server()
             query.name = name
             query.data = data
@@ -106,5 +121,6 @@ if __name__ == '__main__':
                               server_path='data/refined_json_files',
                               wordnet_path='data/wordnet/dictionary_book/final_results',
                               metadata='data/metadata.json',
+                              meta_path='data/json_files/meta',
                               excluded_names=excluded_names)
     initializer()
